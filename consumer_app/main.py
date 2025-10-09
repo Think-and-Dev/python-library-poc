3c2e4fcb8411c02a6539bebb9ba6e2e770edefb8
Nicolas Mendia <nico.mendia@gmail.com>
2025-10-09 13:24:29 -0400

    first commit

---
+++ consumer_app/main.py
@@ -0,0 +1,8 @@
+from adder import add
+from subtractor import subtract
+
+result_add = add(10, 5)
+result_sub = subtract(10, 5)
+
+print(f"Addition result: {result_add}")
+print(f"Subtraction result: {result_sub}")
